48ca5e8de76fbc4496e94c405e8b9657e80da7bf
Nor Gelera <norgelera@outlook.com>
2019-01-08 23:54:32 +0800

    push to vsts

---
+++ ConsoleTesting/Program.cs
@@ -0,0 +1,54 @@
+﻿using POCO.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example Data Only
+            Employee _employee = new Employee
+            {
+                EmployeeId = 1,
+                FirstName = "Nor",
+                Lastname = "Gelera"
+            };
+
+            // Initialize Mapper
+            IMapper<EmployeeViewModel, Employee> _mapper = new ModelMapper<EmployeeViewModel, Employee>();
+
+            // Map to view-model
+            EmployeeViewModel _employeeViewModel = _mapper.from(_employee);
+        }
+    }
+
+    /// <summary>
+    /// POCO entity based on actual database table
+    /// </summary>
+    public class Employee
+    {
+        [MappedTo("Id")]
+        public long EmployeeId { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        [MappedTo("EmployeeName")]
+        public string FullName
+        {
+            get { return Lastname + ", " + FirstName;  }
+        }
+    }
+
+    /// <summary>
+    /// POCO view-model entity which will be consumed outside of your data layer
+    /// </summary>
+    public class EmployeeViewModel
+    {
+        public long Id { get; set; }
+        public string EmployeeName { get; set; }
+    }
+}
---
+++ POCOMapper/model-mapper.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace POCO.Mapper
+{
+    ///<summary>Map values from S to T and/or vice versa</summary>
+    ///<typeparam name="T">Target Type</typeparam>
+    ///<typeparam name="S">Source Type</typeparam>
+    public interface IMapper<T, S>
+    {
+        ///<summary>Map values from S to T</summary>
+        T from(S source);
+        ///<summary>Map values from T to S</summary>
+        S from(T target);
+        ///<summary>Map list from S to T</summary>
+        IList<T> from(IList<S> source);
+        ///<summary>Map list from T to S</summary>
+        IList<S> from(IList<T> target);
+    }
+
+    /// <summary>Mapper Attribute Target</summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class MappedTo : Attribute
+    {
+        private string lName { get; set; } = string.Empty;
+        private Type lType { get; set; } = typeof(string);
+        /// <summary>Map to field</summary>
+        /// <param name="name">Field Name</param>
+        public MappedTo(string name) { lName = name; }
+        /// <summary>Map to a collection field</summary>
+        /// <param name="name">Field Name</param>
+        /// <param name="individualType">Individual Type for collection object</param>
+        public MappedTo(string name, Type individualType) { lName = name; lType = individualType; }
+        /// <summary></summary>
+        public string Name { get { return lName; } }
+        /// <summary></summary>
+        public Type Type { get { return lType; } }
+    }
+
+    ///<summary>Map values from S to T and/or vice versa</summary>
+    ///<typeparam name="T">Target Type</typeparam>
+    ///<typeparam name="S">Source Type</typeparam>
+    public class ModelMapper<T, S> : IMapper<T, S>
+    {
+        private object map(object toConvert, Type targetType)
+        {
+            object _output = Activator.CreateInstance(targetType);
+            foreach (PropertyInfo _convertProp in toConvert.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+            {
+                foreach (PropertyInfo _outputProp in _output.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance))
+                {
+                    string _propertyName = _convertProp.Name;
+                    // * Get custom attribute name
+                    var _mappedTo = _convertProp.GetCustomAttributes(typeof(MappedTo), true).FirstOrDefault();
+                    if (_mappedTo != null)
+                        _propertyName = ((MappedTo)_mappedTo).Name;
+                    if (_outputProp.Name.Equals(_propertyName))
+                    {
+                        // * Check if Enum
+                        if (_outputProp.GetType().IsEnum)
+                            _outputProp.SetValue(_output, Enum.ToObject(_outputProp.GetType(), _convertProp.GetValue(toConvert)));
+                        // * Check if IEnumerable (eg IList, List)
+                        else if (_outputProp.PropertyType != typeof(string) && typeof(IEnumerable).IsAssignableFrom(_outputProp.PropertyType))
+                        {
+                            var _collection = (IEnumerable)_convertProp.GetValue(toConvert, null);
+                            var _listType = typeof(List<>);
+                            var _constructedListType = _listType.MakeGenericType(((MappedTo)_mappedTo).Type);
+                            var _finalList = (IList)Activator.CreateInstance(_constructedListType);
+                            foreach (object _obj in _collection)
+                            {
+                                // * Call method again to map list objects
+                                object _result = map(_obj, ((MappedTo)_mappedTo).Type);
+                                _finalList.Add(_result);
+                            }
+                            _outputProp.SetValue(_output, _finalList);
+                        }
+                        // * Default
+                        else _outputProp.SetValue(_output, _convertProp.GetValue(toConvert));
+                        break;
+                    }
+                }
+            }
+            return _output;
+        }
+
+        T IMapper<T, S>.from(S source)
+        {
+            return (T)map(source, typeof(T));
+        }
+
+        S IMapper<T, S>.from(T target)
+        {
+            return (S)map(target, typeof(S));
+        }
+
+        IList<T> IMapper<T, S>.from(IList<S> source)
+        {
+            if (!source.Any())
+                return new List<T>();
+            else
+            {
+                IMapper<T, S> _mapper = new ModelMapper<T, S>();
+                return source.Select(_b => _mapper.from(_b)).ToList();
+            }
+        }
+
+        IList<S> IMapper<T, S>.from(IList<T> target)
+        {
+            if (!target.Any())
+                return new List<S>();
+            else
+            {
+                IMapper<T, S> _mapper = new ModelMapper<T, S>();
+                return target.Select(_b => _mapper.from(_b)).ToList();
+            }
+        }
+    }
+}
